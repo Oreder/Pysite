c722d74c059f220a8a9bdb91eebe59bc29d298d1
Oreder <stepup.ored@gmail.com>
2018-10-03 18:16:15 +0300

    add tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+class ViewTest(TestCase):
+	def testAppView(self):
+		resp = self.client.get('/')
+		self.assertEqual(resp.status_code, 200)
+	
+	def testProfile(self):
+		resp = self.client.post('/', {'user': 'Oreder'} )
+		self.assertEqual(resp.status_code, 200)
+
+	def testProfileContent(self):
+		resp = self.client.post('/', {'user': 'sherk'} )
+		self.assertNotEqual(resp.content, '')
